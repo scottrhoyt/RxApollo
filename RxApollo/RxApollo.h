30014459dd495a054836c5a1d9cd0ff1d3faac98
Scott Hoyt <scottrhoyt@gmail.com>
2017-05-09 01:47:45 -0700

    Initial Commit

---
+++ RxApollo/RxApollo.h
@@ -0,0 +1,19 @@
+//
+//  RxApollo.h
+//  RxApollo
+//
+//  Created by Scott Hoyt on 5/9/17.
+//  Copyright © 2017 Scott Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxApollo.
+FOUNDATION_EXPORT double RxApolloVersionNumber;
+
+//! Project version string for RxApollo.
+FOUNDATION_EXPORT const unsigned char RxApolloVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxApollo/PublicHeader.h>
+
+
